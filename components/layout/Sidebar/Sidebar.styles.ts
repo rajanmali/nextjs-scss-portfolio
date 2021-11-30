3c8984d4712abd141c9a41a832565b112a1b5e0c
Rajan Mali <mysteriouskiller50@gmail.com>
2021-11-30 11:03:54 +0545

    added basic structure for sidebar

--- components/layout/Sidebar/Sidebar.styles.ts
+++ components/layout/Sidebar/Sidebar.styles.ts
@@ -7,4 +7,22 @@ export const SidebarWrapper = styled.div`
 
   width: 20%;
   margin: 0;
+
+  padding: 0 20px;
+
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: flex-start;
 `;
+
+export const SidebarLogo = styled.div``;
+export const SidebarNavWrapper = styled.div``;
+export const SidebarNavMenu = styled.div``;
+export const SidebarMenuItem = styled.div``;
+export const SidebarSocialWrapper = styled.div`
+  display: flex;
+`;
+export const SidebarSocialIcon = styled.div``;
+export const SidebarFooter = styled.div``;
+export const SidebarFooterHeading = styled.div``;
--- components/layout/Sidebar/index.tsx
+++ components/layout/Sidebar/index.tsx
@@ -1,10 +1,42 @@
 import { FC } from 'react';
 
 // Styles
-import { SidebarWrapper } from './Sidebar.styles';
+import {
+  SidebarWrapper,
+  SidebarLogo,
+  SidebarNavWrapper,
+  SidebarNavMenu,
+  SidebarMenuItem,
+  SidebarSocialWrapper,
+  SidebarSocialIcon,
+  SidebarFooter,
+  SidebarFooterHeading,
+} from './Sidebar.styles';
 
 const Sidebar: FC = () => {
-  return <SidebarWrapper>Sidebar</SidebarWrapper>;
+  return (
+    <SidebarWrapper>
+      <SidebarLogo />
+      <SidebarNavWrapper>
+        <SidebarNavMenu>
+          <SidebarMenuItem>Home</SidebarMenuItem>
+          <SidebarMenuItem>About</SidebarMenuItem>
+          <SidebarMenuItem>Profile</SidebarMenuItem>
+          <SidebarMenuItem>Contact</SidebarMenuItem>
+        </SidebarNavMenu>
+      </SidebarNavWrapper>
+      <SidebarSocialWrapper>
+        <SidebarSocialIcon>Twitter</SidebarSocialIcon>
+        <SidebarSocialIcon>Github</SidebarSocialIcon>
+        <SidebarSocialIcon>Medium</SidebarSocialIcon>
+      </SidebarSocialWrapper>
+      <SidebarFooter>
+        <SidebarFooterHeading>
+          Copyright {new Date().getFullYear()} | Rajan Mali
+        </SidebarFooterHeading>
+      </SidebarFooter>
+    </SidebarWrapper>
+  );
 };
 
 export default Sidebar;
